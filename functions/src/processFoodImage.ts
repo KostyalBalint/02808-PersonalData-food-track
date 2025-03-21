0aac84f5e412f7e0e8d7ab5e3c31842091ef5795
KostyalBalint <kostyal.balint@icloud.com>
2025-03-21 13:25:13 +0100

    feat: analyze food image

--- functions/src/processFoodImage.ts
+++ functions/src/processFoodImage.ts
@@ -1,20 +1,35 @@
 import { onDocumentCreated } from "firebase-functions/v2/firestore";
+import { genkit } from "genkit";
+import { googleAI, gemini20Flash } from "@genkit-ai/googleai";
 
 // @ts-expect-error Only type import
 import { MealData } from "../../src/constants";
+import { FoodExtractSchema } from "./foodExtractSchema.js";
 
-exports.documentCreatedHandler = onDocumentCreated(
+export const documentCreatedHandler = onDocumentCreated(
   "meals/{mealId}",
-  (event) => {
-    event.data?.ref.set(
+  async (event) => {
+    const ai = genkit({ plugins: [googleAI()] });
+    const meal = event.data?.data() as MealData;
+
+    const { output } = await ai.generate({
+      system:
+        "Analyze the given food image and collect all the ingredient that the dish is made out of",
+      model: gemini20Flash,
+      prompt: { media: { url: meal.imageUrl } },
+      output: {
+        schema: FoodExtractSchema,
+      },
+    });
+
+    return event.data?.ref.set(
       {
-        ingredients: [
-          {
-            amount: 0,
-            name: "NA",
-            unit: "NA",
-          },
-        ],
+        ingredients: output?.ingredients.map((ingredient, index) => ({
+          id: index,
+          name: ingredient.name,
+          amount: ingredient.amount,
+          unit: ingredient.unit,
+        })),
       } as Partial<MealData>,
       {
         merge: true,
