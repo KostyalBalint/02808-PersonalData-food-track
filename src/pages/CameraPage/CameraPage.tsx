f596a7f7822805d472eb863e51d16e6206cf5278
KostyalBalint <kostyal.balint@icloud.com>
2025-04-07 22:58:05 +0200

    feat: camera resolution bump

--- src/pages/CameraPage/CameraPage.tsx
+++ src/pages/CameraPage/CameraPage.tsx
@@ -93,7 +93,8 @@ export const CameraPage: React.FC = () => {
 
       // Get new stream
       const stream = await navigator.mediaDevices.getUserMedia({
-        video: { facingMode },
+        video: { facingMode, height: { ideal: 2048 }, width: { ideal: 2048 } },
+        //video: { facingMode },
         audio: false,
       });
 
@@ -150,7 +151,7 @@ export const CameraPage: React.FC = () => {
         }
 
         new Compressor(blob, {
-          quality: 0.8,
+          quality: 0.95,
           maxWidth: 2048,
           maxHeight: 2048,
           resize: "contain",
